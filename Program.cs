d229a5196a3b5049d4544ae0c1af977915a2c945
UllaRaaschou <126786969+UllaRaaschou@users.noreply.github.com>
2026-03-02 14:00:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ClosedXML.Excel;
+
+
+
+Console.Write("Indtast filsti: ");
+var input = Console.ReadLine();
+var filePath = FileHelper.GetInputPath(input);
+Console.WriteLine(filePath);
+var workbook = new XLWorkbook(filePath);
+
+
+
+
+public static class FileHelper
+{
+    public static string GetInputPath()
+    {
+        string? inputPath = null;
+        try
+        {
+            inputPath = Console.ReadLine();
+        }
+        catch 
+        { 
+            throw new IOException("It was not possible to read inputPath")
+        }
+        var lastIndex = inputPath.Length - 1;
+        if (inputPath[0] == '"' && inputPath[lastIndex] == '"')
+        {
+            inputPath = inputPath.Trim('"');
+        }
+
+        return inputPath;
+    }
+}
