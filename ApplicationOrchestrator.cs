333d9c5fa69a26e37907e2cd5628aadff2646c2d
UllaRaaschou <126786969+UllaRaaschou@users.noreply.github.com>
2026-03-12 13:35:53 +0100

    With ApplicationOrcestrator

--- ApplicationOrchestrator.cs
+++ ApplicationOrchestrator.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PDFDownloader.Data;
+using Serilog;
 
 namespace PDFDownloader
 {
     public class ApplicationOrchestrator
     {
+        private readonly DataAccessService _dataAccess;
+        private readonly DownloadPreparer _preparer;
+        private readonly DownloadService _downloadService;
+        private readonly IUniversalDownloadedFiles _universalDownloadedFiles;
+
+        public ApplicationOrchestrator(
+            DataAccessService dataAccess,
+            DownloadPreparer preparer,
+            DownloadService downloadService,
+            IUniversalDownloadedFiles universalDownloadedFiles)
+        {
+            _dataAccess = dataAccess;
+            _preparer = preparer;
+            _downloadService = downloadService;
+            _universalDownloadedFiles = universalDownloadedFiles;
+        }
+
+        public async Task<bool> RunAsync()
+        {
+            try
+            {                
+                var workbook = _dataAccess.CreateWorkbook(); 
+                var worksheet = _dataAccess.AccessWorkSheet(workbook);  
+
+              
+                if (workbook == null || worksheet == null)
+                {
+                    Console.WriteLine("Fejl: Kunne ikke åbne Excel-fil eller arbejdsark");
+                    return false;
+                }
+                               
+                var downloadFolder = _dataAccess.DownloadFolder;
+                var wantCheck = _dataAccess.WantCheckForFormerDownloads();
+                             
+                Console.WriteLine("Download starter, tag en kop kaffe");
+                var notDownloadedBefore = _preparer.PrepareForDownload(worksheet, downloadFolder, wantCheck);
+                          
+                await _downloadService.DownloadAllAsync(notDownloadedBefore, downloadFolder);
+
+                Console.WriteLine("Starting WriteToExcel...");
+                var writer = new ExcelWriter(
+                    downloadFolder,
+                    _downloadService._failedDownloads,
+                    _downloadService._succeededDownloads);
+                await writer.WriteToExcel(downloadFolder);
+                Console.WriteLine("WriteToExcel completed!");
+
+                return true;
+            }
+            catch (IOException ex)
+            {               
+                Log.Error(ex, "IO fejl: {Message}", ex.Message);
+                Console.WriteLine($"Fejl: {ex.Message}");
+                return false;
+            }
+            catch (Exception ex)
+            {                
+                Log.Error(ex, "Uventet fejl under download proces");
+                Console.WriteLine($"Der opstod en uventet fejl: {ex.Message}");
+                return false;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,52 +1,28 @@
-using System.Threading.Tasks;
 using Serilog;
-using System;
-using System.IO;
-using PDFDownloader.Services;
-using static DownloadPreparer;
 using PDFDownloader.Data;
-using Serilog.Sinks.File;
 using PDFDownloader; // Add this using directive for Serilog File sink
-
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.File("logs/download.log")
-    .CreateLogger();
-
-
-var access = new DataAccessService();
-var workbook = access.CreateWorkbook();
-var accessedWorksheet = access.AccessWorkSheet(workbook);
-
-if (workbook == null || accessedWorksheet == null)
+internal class Program
 {
-    Log.Error("Failed to initialize workbook or worksheet");
-    Console.WriteLine("Fejl: Kunne ikke åbne Excel-fil eller arbejdsark");
-    Console.ReadLine();
-    return;
+    private static async Task Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+        .WriteTo.File("logs/download.log")
+        .CreateLogger();
+        var uni = new UniversalDownloadedFiles();
+        var httpClient = new HttpClient();
+        var dataAccess = new DataAccessService();
+        var preparer = new DownloadPreparer(uni, dataAccess);
+        var downloadService = new DownloadService(uni, httpClient);
+        var orchestrator = new ApplicationOrchestrator(
+            dataAccess,
+            preparer,
+            downloadService,
+            uni);
+        var success = await orchestrator.RunAsync();
+        if (success)
+        {
+            Console.WriteLine("Done!!!");
+        }
+        Console.ReadLine();
+    }
 }
-
-
-var uni = new UniversalDownloadedFiles();
-var httpClient = new HttpClient();
-
-
-
-var preparer = new DownloadPreparer(uni, access);
-
-var downloadFolder = access.DownloadFolder;
-var wantCheck = access.WantCheckForFormerDownloads();
-Console.WriteLine("Download starter, tag en kop kaffe");
-var notDownloadedBefore = preparer.PrepareForDownload(accessedWorksheet, downloadFolder, wantCheck);
-
-var httpDownloadService = new DownloadService(uni, httpClient);
-await httpDownloadService.DownloadAllAsync(notDownloadedBefore, downloadFolder);
-
-Console.WriteLine("Starting WriteToExcel...");
-var writer = new ExcelWriter(downloadFolder, httpDownloadService._failedDownloads, httpDownloadService._succeededDownloads);
-await writer.WriteToExcel(downloadFolder);
-Console.WriteLine("WriteToExcel completed!");
-//Console.WriteLine(Path.GetTempPath());
-Console.ReadLine();
-Console.WriteLine("Done!!!");
-Console.ReadLine();
-
